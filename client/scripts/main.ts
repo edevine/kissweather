0f5f5e1965cb37bfd46e7278fbcd649c8f67d8ed
Eric Devine <devineej@gmail.com>
2016-02-17 00:18:44 -0500

    Implement simple daily and hourly forecast tables

--- client/scripts/main.ts
+++ client/scripts/main.ts
@@ -1,6 +1,18 @@
-const zipCodeInput: HTMLInputElement = <any> document.getElementById('zipcode');
-const locationOutput: HTMLElement = document.getElementById('location');
-const currentTempOutput: HTMLElement = document.getElementById('current-temp');
+const secondsInDay = 24 * 60 * 60;
+
+const zipCodeInput = document.getElementById<HTMLInputElement>('zipcode');
+const locationOutput = document.getElementById<HTMLElement>('location');
+const currentTempOutput = document.getElementById<HTMLElement>('current-temp');
+
+const dailyForecastTable = document.getElementById<HTMLTableElement>('daily-forecast');
+const dailyForecastTableBody = dailyForecastTable.querySelector<HTMLBodyElement>('tbody');
+const dailyForecastRowTemplate = dailyForecastTableBody.querySelector<HTMLTableRowElement>('tr');
+dailyForecastRowTemplate.parentNode.removeChild(dailyForecastRowTemplate);
+
+const hourlyForecastTable = document.getElementById<HTMLTableElement>('hourly-forecast');
+const hourlyForecastTableBody = hourlyForecastTable.querySelector<HTMLBodyElement>('tbody');
+const hourlyForecastRowTemplate = hourlyForecastTableBody.querySelector<HTMLTableRowElement>('tr');
+hourlyForecastRowTemplate.parentNode.removeChild(hourlyForecastRowTemplate);
 
 zipCodeInput.addEventListener('change', () => {
     let zipCode = zipCodeInput.value;
@@ -17,5 +29,50 @@ zipCodeInput.addEventListener('change', () => {
                 currentTempOutput.textContent = '';
             }
         });
+        
+        fetch('http://kissweather.com/data/forecast/daily?zip=' + zipCode + ',us')
+        .then(response => response.json())
+        .then((forecast: DailyForecast) => {
+            dailyForecastTableBody.innerHTML = "";
+            let lastDate = '';
+            for (let i = 0; i < forecast.list.length; i++) {
+                let row = dailyForecastRowTemplate.cloneNode(true);
+                row.children[0].textContent = toDateStamp(forecast.list[i].dt);
+                row.children[1].textContent = toFahrenheit(forecast.list[i].temp.max).toFixed(0) + ' / ' + toFahrenheit(forecast.list[i].temp.min).toFixed(0);
+                dailyForecastRowTemplate.appendChild(row);
+            }
+        });
+        
+        fetch('http://kissweather.com/data/forecast?zip=' + zipCode + ',us')
+        .then(response => response.json())
+        .then((forecast: HourlyForecast) => {
+            hourlyForecastTableBody.innerHTML = "";
+            let lastDate = '';
+            for (let i = 0; i < forecast.list.length; i++) {
+                let row = hourlyForecastRowTemplate.cloneNode(true);
+                
+                let [date, time] = forecast.list[i].dt_txt.split(' ');
+                if (lastDate !== date) {
+                    lastDate = date;
+                    row.children[0].textContent = date;
+                }
+                row.children[1].textContent = time;
+                row.children[2].textContent = toFahrenheit(forecast.list[i].main.temp).toFixed(0);
+                hourlyForecastTable.appendChild(row);
+            }
+        });
     }
 });
+
+function toFahrenheit(kelvin: number) {
+    return kelvin * (9 / 5) - 459.67;
+}
+
+function isMidnight(utcSeconds: number) {
+    return utcSeconds % secondsInDay === 0;
+}
+
+function toDateStamp(utcSeconds: number) {
+    let date = new Date(utcSeconds * 1000);
+    return String(date.getFullYear()) + '-' + String(date.getMonth() + 1) + '-' + String(date.getDate());
+}
---
+++ client/scripts/typings/dom.d.ts
@@ -0,0 +1,16 @@
+interface Document {
+    getElementById<Node extends HTMLElement>(elementId: string): Node;
+}
+
+interface NodeSelector {
+    querySelector<Node extends Element>(selectors: string): Node;
+    querySelectorAll<Node extends Element>(selectors: string): NodeListOf<Node>;
+}
+
+interface Element {
+    cloneNode(deep?: boolean): Element;
+}
+
+interface HTMLElement {
+    cloneNode(deep?: boolean): HTMLElement;
+}
